ec546d9c49169c420a0ccedc86fb4003469fd36a
camille-emel <camille.emel@le-campus-numerique.fr>
2024-08-28 16:50:19 +0200

    update

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -6,55 +6,50 @@ class Magasin {
     public Magasin(Item[] items) {
         this.items = items;
     }
-
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Comté")
-                    && !items[i].name.equals("Pass VIP Concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Kryptonite")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
+            //Comté
+            if (items[i].name.equals("Comté")) {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                    if (items[i].sellIn > 0) {
+                        items[i].quality += 1;
+                    }
+                    else {
+                        items[i].quality += 2;
                     }
                 }
             }
-
-            if (!items[i].name.equals("Kryptonite")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            //Pass VIP
+            else if (items[i].name.equals("Pass VIP Concert")) {
+                if (items[i].quality < 50) {
+                    if (items[i].sellIn <= 0) {
+                        items[i].quality = 0;
+                    }
+                    else if (items[i].sellIn < 6) {
+                        items[i].quality += 3;
+                    }
+                    else if (items[i].sellIn < 11) {
+                        items[i].quality += 2;
+                    }
+                    else {
+                        items[i].quality += 1;
+                    }
+                }
             }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Comté")) {
-                    if (!items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Kryptonite")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            //Kryptonite
+            else if (items[i].name.equals("Kryptonite")) {
+                items[i].quality = 80;
+            }
+            //All others products
+            else {
+                if (items[i].sellIn > 0 ) {
+                    items[i].quality -= 1;
+                }
+                else if (items[i].sellIn <= 0 ) {
+                    items[i].quality -= 2;
+                }
+                if (items[i].quality < 0) {
+                    items[i].quality = 0;
                 }
             }
         }
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,11 +7,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void comteday10() {
+        Item[] items = new Item[] { new Item("Comté", 10, 10) };
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(11, app.items[0].quality);
     }
-
+    @Test
+    void comteday1() {
+        Item[] items = new Item[] { new Item("Comté", 1, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+    @Test
+    void comteday0() {
+        Item[] items = new Item[] { new Item("Comté",0 , 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+    }
+    @Test
+    void kryptoniteday10() {
+        Item[] items = new Item[] { new Item("Kryptonite", 10, 80) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+    }
+    @Test
+    void kryptoniteday1() {
+        Item[] items = new Item[] { new Item("Kryptonite", 1, 80) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+    }
+    @Test
+    void vipday11() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 11, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+    @Test
+    void vipday10() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 10, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+    }
+    @Test
+    void vipday5() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 5, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(13, app.items[0].quality);
+    }
+    @Test
+    void vipday0() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 0, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+    @Test
+    void trucday10() {
+        Item[] items = new Item[] { new Item("Truc", 10, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].quality);
+    }
+    @Test
+    void trucday0() {
+        Item[] items = new Item[] { new Item("Truc", 0, 10) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
 }
